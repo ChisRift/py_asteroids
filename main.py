ebc351f181983809919e082bed8becf5d281a0ee
chisriftgames <chisrift51@gmail.com>
2025-07-03 16:24:33 +0100

    Player data finished

--- main.py
+++ main.py
@@ -13,16 +13,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while(True):
         # check events for quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updateable.update(dt)
         screen.fill(color="black")
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
